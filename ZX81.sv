c3651cb2b7e71b8d7ed6bb74c2257448b946d3e3
Timothy Redaelli <timothy.redaelli@gmail.com>
2026-06-09 15:34:37 +0200

    DB9: consume programmable remap matrix at joystick merge

--- ZX81.sv
+++ ZX81.sv
@@ -385,8 +385,8 @@ wire        forced_scandoubler;
 wire [21:0] gamma_bus;
 
 // F4 F3 F2 F1 U D L R 
-wire [31:0] joystick_0 = joydb_1ena ? (OSD_STATUS? 32'b000000 : joydb_1[7:0]) : joystick_0_USB;
-wire [31:0] joystick_1 = joydb_2ena ? (OSD_STATUS? 32'b000000 : joydb_2[7:0]) : joydb_1ena ? joystick_0_USB : joystick_1_USB;
+wire [31:0] joystick_0 = joydb_1ena ? (OSD_STATUS? 32'b000000 : joydb_1_mapped[7:0]) : joystick_0_USB;
+wire [31:0] joystick_1 = joydb_2ena ? (OSD_STATUS? 32'b000000 : joydb_2_mapped[7:0]) : joydb_1ena ? joystick_0_USB : joystick_1_USB;
 
 
 
